02ef24eaf7b13eb9b27cd5ba7c60c8d0ec354aec
LAPTOP-274B45R1\prave <praveensingh286@gmail.com>
2022-06-09 13:56:08 +0530

    WNAY-1 create admin project

--- 99watt.WEB/src/app/app-routing.module.ts
+++ 99watt.WEB/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'home', component:HomeComponent, pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- 99watt.WEB/src/app/app.component.ts
+++ 99watt.WEB/src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '99watt.WEB';
+  a='b';
+  b='a';
 }
